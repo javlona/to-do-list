b43c4da2efc0d68c2dc37d77b76abe50841af66e
Javlon <59303287+javlona@users.noreply.github.com>
2021-11-18 07:16:58 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+let inputText = document.querySelector('.input-text');
+let buttonAdd = document.querySelector('.button-add');
+let ul = document.querySelector('ul');
+let checkbox = '<input type="checkbox" class="checkbox">';
+let deleteIcon = '<i class="fas fa-trash-alt delete-one"></i>'
+
+let addText = () => {
+    if(inputText.value===""){
+        return alert('field cannot be empty')
+    }
+    
+    let li = document.createElement('li')
+    li.innerHTML = checkbox + inputText.value + deleteIcon;
+    ul.appendChild(li);
+    inputText.value = ""
+}
+
+// let deleteList = (e) => {
+//     e.remove();
+// }
+
+let addTextOnSpace = (e) => {
+    if(e === 13) addText();
+}
+
+inputText.addEventListener('keyup', addTextOnSpace);
+buttonAdd.addEventListener('click', addText);
