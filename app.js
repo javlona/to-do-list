9f006f1259977b6caf5ee4600b252f61677d854a
javlona <joeclaap@gmail.com>
2021-11-18 23:41:49 -0600

    delete all functionality

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ let ul = document.querySelector('ul');
 let checkbox = '<input type="checkbox" class="checkbox">';
 let deleteIcon = '<i onclick="deleteSingleItem(event)" class="fas fa-trash-alt delete-one"></i>'
 let li = document.querySelector('li')
+let deleteAll = document.querySelector('.delete-all')
 
 
+// methods to easily access localStorage
 let storage = {
     add: function(key, value){
         return localStorage.setItem(key, JSON.stringify(value))
@@ -19,14 +21,13 @@ let storage = {
 };
 
 
-
-//add list item from input
+// add list item from input
 let addText = () => {
-    //check if it not empty
+    // check if it not empty
     if(inputText.value===""){
         return alert('field cannot be empty')
     }
-    //
+    // add values from inputText
     let todo = storage.get('todo')
     let todoArr = [];
     if(!todo) {
@@ -36,6 +37,7 @@ let addText = () => {
     }
     storage.add('todo', todoArr)
     
+    // push values into ul element
     let li = document.createElement('li')
     let newTodo = storage.get('todo')
     li.innerHTML = checkbox + newTodo[newTodo.length-1] + deleteIcon;
@@ -43,7 +45,7 @@ let addText = () => {
     inputText.value = ""
 }
 
-//remove list item individually on delete icon
+// remove list item individually on delete icon
 let deleteSingleItem = (e) => {
     e.target.parentElement.remove()
     let todos = storage.get('todo')
@@ -54,11 +56,18 @@ let deleteSingleItem = (e) => {
     storage.add("todo", todos);
 }
 
-//fire action on event
+// fire action on event
 let addTextOnButton = () => addText();
 let addTextOnSpace = (e) => {
     if(e.keyCode === 13) addText();
 }
 
+// delete all li element inside ul and clear localStorage
+let deleteAllButton = () => {
+    ul.innerHTML = "";
+    localStorage.clear();
+}
+
+deleteAll.addEventListener('click', deleteAllButton);
 inputText.addEventListener('keyup', addTextOnSpace);
 buttonAdd.addEventListener('click', addTextOnButton);
